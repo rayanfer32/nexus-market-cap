38e0d40cc10add25dded6426479a3c533db04b74
shrivatsaBhat <shrivatsabhatpolali@gmail.com>
2022-07-02 16:30:24 +0530

    ci: dont with testing

--- dangerfile.ts
+++ dangerfile.ts
@@ -34,27 +34,7 @@ if (modified_files.length > 0) {
   message('Changed Files in this PR: \n <ol><li>' + modifiedMD + '</li></ol>')
 }
 
-if (pr.base.ref === 'main') {
-  fail('Please specify branch to continue with PR - ' + pr.base.ref)
-}
-if (pr.base.ref == 'develop') {
-  fail(
-    'develop branch - ' +
-      pr.base.ref +
-      '\n================================' +
-      pr.changed_files +
-      '================================' +
-      pr.head.label +
-      ',' +
-      pr.head.ref +
-      ',' +
-      danger.git.base +
-      '***' +
-      danger.github.thisPR.owner
-  )
-}
-
-if (pr.changed_files > 20) {
+if (pr.changed_files > 10 && pr.base.ref !== 'main') {
   // check branch is raise to main
   warn(
     'This PR has a lot of changes. Please make sure you have a good reason to do this.'
