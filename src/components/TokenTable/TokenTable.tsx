ac594e019f8b581c1fac63a38947ac9f15b409c0
Rayan fernandes <37145078+Rayanfer32@users.noreply.github.com>
2022-07-04 22:08:42 +0530

    fix: tickerbar , tokentable

--- src/components/TokenTable/TokenTable.tsx
+++ src/components/TokenTable/TokenTable.tsx
@@ -7,11 +7,12 @@ import nexusTokenBgLight from '../../assets/icons/TokenIconType1.svg'
 // import tokenbgLight from '@assets/icons/tokenbgLight.svg'
 
 import Image from 'next/image'
+import { Button } from '@components/common/Button'
 interface tokenData {
   token: string
   ticker: string
   maxsupply: number
-  currentSupply: number
+  currentsupply: number
   owner: string
   decimals: number
   balance: number
@@ -37,12 +38,16 @@ export default function TokenTable() {
           <tr>
             <th>#</th>
             <th>Token</th>
-            <th>Price</th>
-            <th>24h</th>
-            <th>7D</th>
+
+            {/* // ! no api support */}
+            {/* <th>Price</th> */}
+            {/* <th>24h</th> */}
+            {/* <th>7D</th> */}
+            {/* <th>Volume(24h)</th> */}
+
+            <th>Balance</th>
             <th>MaxSupply</th>
             <th>CurrentSupply</th>
-            <th>Volume(24h)</th>
           </tr>
         </thead>
 
@@ -51,17 +56,23 @@ export default function TokenTable() {
             <tr key={index} className={styles.tr}>
               <td>{index + 1}</td>
               <td>
-                {item?.ticker
-                  ? makeIcon(removeLocalTag(item.ticker))
-                  : item.token.substring(0, 10)}
+                {item?.ticker ? (
+                  makeIcon(removeLocalTag(item.ticker.substring(0, 10)))
+                ) : (
+                  <div className={styles.token_name}>
+                    {item.token.substring(0, 10)}
+                  </div>
+                )}
                 {/* {removeLocalTag(item.ticker)} */}
               </td>
-              <td>3.2$</td>
-              <td>+32.5%</td>
-              <td>+21.2%</td>
+
+              {/* <td>3.2$</td> */}
+              {/* <td>+32.5%</td> */}
+              {/* <td>+21.2%</td> */}
+
+              <td>{item.balance.toFixed(1)}</td>
               <td>{item.maxsupply}</td>
-              <td>{item.currentSupply}</td>
-              <td>{item.balance}</td>
+              <td>{item.currentsupply.toFixed(1)}</td>
             </tr>
           ))}
         </tbody>
@@ -87,14 +98,14 @@ function makeIcon(name: string): React.ReactElement {
   )
 }
 
-function removeLocalTag(name: string) {
+function removeLocalTag(name: string): string {
   return name?.replace('local:', '')
 }
 
 function ActionBar() {
   return (
-    <div className="p-2">
-      <button className="btn btn-sm">lock</button>
+    <div className="">
+      <Button>Lock</Button>
     </div>
   )
 }
