b653be4a42caa13b96e399f28f91bcfc432aa29d
shrivatsabhat <shrivatsabhatpolali@gmail.com>
2022-07-26 23:26:25 +0530

    feat: get string from route link

---
+++ src/utils/parseLink.ts
@@ -0,0 +1,9 @@
+/**
+ * Get string from a route link
+ * @param path  the path of the route
+ * @returns route string
+ */
+export const getRouteName = (path: string): string => {
+  const route = path.replace(/\/|#/, '')
+  return route ? route : '';
+}
