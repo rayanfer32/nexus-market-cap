ea84084ad9bf0d4a8ef62959b251b368467c7818
shrivatsaBhat <shrivatsabhatpolali@gmail.com>
2022-07-27 22:43:53 +0530

    feat: add brand component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,19 +1,14 @@
-import Image from 'next/image'
 import React from 'react'
-import NexusBlueSvg from '@assets/icons/nexusBlue.svg'
 import { BiMenu, BiSearch } from 'react-icons/bi'
 import { IconContext } from 'react-icons'
 import styles from './Header.module.scss'
+import Brand from '@components/common/Brand'
 
 function Header() {
   return (
     <div>
       <nav className={styles.nav}>
-        <div className={styles.logo}>
-          <Image src={NexusBlueSvg} width={32} height={32} alt="logo" />
-          <p className={styles.title}>NexusMarketCap</p>
-        </div>
-
+        <Brand />
         <div className={styles.nav_buttons}>
           <IconContext.Provider value={{ size: '1.5rem' }}>
             <BiSearch />
