77789ed67b0212d70590f23a5b62a0537c8b7ff0
Rayan fernandes <37145078+Rayanfer32@users.noreply.github.com>
2022-07-30 15:11:35 +0530

    feat: add react carousal for ads

--- src/components/Advertisement/Advertisement.tsx
+++ src/components/Advertisement/Advertisement.tsx
@@ -2,13 +2,41 @@ import React from 'react'
 import styles from './Advertisement.module.scss'
 import adsData from '@assets/data/adsData.json'
 import Image from 'next/image'
-
+import Carousel, { RenderArrowProps } from 'react-elastic-carousel'
+import { HiArrowSmLeft, HiArrowSmRight } from 'react-icons/hi'
 export default function Advertisement() {
+  const breakPoints = [
+    { width: 1, itemsToShow: 1 },
+    { width: 550, itemsToShow: 2, itemsToScroll: 2, pagination: false },
+    { width: 850, itemsToShow: 3 },
+    { width: 1150, itemsToShow: 4, itemsToScroll: 2 },
+    { width: 1450, itemsToShow: 5 },
+    { width: 1750, itemsToShow: 6 },
+  ]
+
+  function myArrow({ type, onClick, isEdge }: RenderArrowProps) {
+    const Pointer = type === 'PREV' ? HiArrowSmLeft : HiArrowSmRight
+    return (
+      <button style={{ border: '0' }} onClick={onClick} disabled={isEdge}>
+        <Pointer />
+      </button>
+    )
+  }
+
   return (
     <div className={styles.container}>
-      {adsData.map(({ title, desc, image }) => (
-        <AdCard key={title} title={title} desc={desc} image={image} />
-      ))}
+      <Carousel
+        itemPadding={[0, 150]}
+        renderArrow={myArrow}
+        breakPoints={breakPoints}
+        enableAutoPlay
+        autoPlaySpeed={3000}
+        itemsToShow={3}
+      >
+        {adsData.map(({ title, desc, image }) => (
+          <AdCard key={title} title={title} desc={desc} image={image} />
+        ))}
+      </Carousel>
     </div>
   )
 }
@@ -22,7 +50,7 @@ interface AdCardProps {
 function AdCard({ title, desc, image, ...rest }: AdCardProps) {
   return (
     <div className={styles.card} {...rest}>
-      <span className={styles.card_title}>{title}</span>
+      <div className={styles.card_title}>{title}</div>
       <div className={styles.card_body}>
         <Image
           className={styles.card_body_image}
@@ -31,7 +59,7 @@ function AdCard({ title, desc, image, ...rest }: AdCardProps) {
           width={300}
           height={200}
         />
-        <span className={styles.card_body_desc}>{desc}</span>
+        <div className={styles.card_body_desc}>{desc}</div>
       </div>
     </div>
   )
