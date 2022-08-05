b68a1aba5c160fa9c4b1d1e618301bfd4e2bb38a
Rayan fernandes <37145078+Rayanfer32@users.noreply.github.com>
2022-08-05 21:39:29 +0530

    fix: missing Head tag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,8 +1,9 @@
-import { Html, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document'
 
 const Document = () => {
   return (
     <Html lang="en">
+      <Head />
       <body data-theme="light" className="dark">
         <Main />
         <NextScript />
