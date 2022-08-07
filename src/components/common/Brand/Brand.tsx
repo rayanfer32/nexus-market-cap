3b99ec187767b0cc0caff09991ab83244834a7b0
shrivatsaBhat <shrivatsabhatpolali@gmail.com>
2022-08-07 22:44:26 +0530

    refactor: add updated logo

--- src/components/common/Brand/Brand.tsx
+++ src/components/common/Brand/Brand.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-import NexusBlueSvg from '@assets/icons/nexusBlue.svg'
+import NMC from '@assets/icons/nmc_light.svg'
 import styles from './Brand.module.scss'
 import CustomLink from '../CustomLink'
 import routes from '@constants/routes'
@@ -7,13 +7,7 @@ import routes from '@constants/routes'
 export default function Brand() {
   return (
     <CustomLink href={routes.HOME} className={styles.logo}>
-      <Image
-        src={NexusBlueSvg}
-        width={32}
-        height={32}
-        alt="logo"
-        loading="eager"
-      />
+      <Image src={NMC} width={32} height={32} alt="logo" loading="eager" />
       <p className={styles.title}>NexusMarketCap</p>
     </CustomLink>
   )
