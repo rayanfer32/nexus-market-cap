83b3b1cf0d244cb70d0dd5d62f3fc35dc45d67ab
shrivatsaBhat <shrivatsabhatpolali@gmail.com>
2022-08-08 10:16:10 +0530

    feat: search bar component

--- src/components/common/Input/Input.tsx
+++ src/components/common/Input/Input.tsx
@@ -1,10 +1,18 @@
+import { cls } from '@utils/classnames'
 import { InputHTMLAttributes } from 'react'
-// import styles from './Input.module.scss';
+import styles from './Input.module.scss'
 
-interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   varient?: 'default' | 'outline' | 'ghost'
+  className?: string
 }
 
 export const Input = (props: InputProps) => {
-  return <input {...props} />
+  const { varient = 'default', className = '' } = props
+  return (
+    <input
+      {...props}
+      className={cls(styles.input, styles[varient], className)}
+    />
+  )
 }
---
+++ src/components/common/SearchBar/SearchBar.tsx
@@ -0,0 +1,20 @@
+import { cls } from '@utils/classnames'
+import React from 'react'
+import { BiSearch } from 'react-icons/bi'
+import { InputProps } from '../Input'
+import styles from './SearchBar.module.scss'
+
+export interface SearchBarProps extends InputProps {}
+
+export const SearchBar = (props: SearchBarProps) => {
+  return (
+    <div className={cls(styles.bar)}>
+      <input {...props} type={'search'} className={cls(styles.search)} />
+      <button type="submit" className={styles.icon}>
+        <BiSearch color="inherit" />
+      </button>
+    </div>
+  )
+}
+
+export default SearchBar
