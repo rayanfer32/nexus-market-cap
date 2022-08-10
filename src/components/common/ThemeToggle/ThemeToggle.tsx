5ea6994439358d6001467b296e68352b722e7331
shrivatsaBhat <shrivatsabhatpolali@gmail.com>
2022-08-10 12:40:20 +0530

    fix: theme icon is miss match with theme

--- src/components/common/ThemeToggle/ThemeToggle.tsx
+++ src/components/common/ThemeToggle/ThemeToggle.tsx
@@ -5,26 +5,45 @@ import { BsMoonFill } from 'react-icons/bs'
 import { MdDevices } from 'react-icons/md'
 import { useDarkMode } from '@utils/hooks'
 import { theme } from '@constants/theme'
+import { memo, useCallback, useEffect, useRef } from 'react'
+import { useMediaQuery } from '@utils/hooks/useMediaQuery'
 
 export type ThemeType = 'light' | 'dark' | 'system'
+const mq = '(prefers-color-scheme: dark)'
 
 export const ThemeToggle = () => {
+  const Icon = useRef(MdDevices)
   const [darkMode, setDarkMode] = useDarkMode()
+  const prefersDarkMode = useMediaQuery(mq)
 
-  const onClick = () => {
-    const newMode = darkMode === theme.DARK ? theme.LIGHT : theme.DARK
-    setDarkMode(newMode)
-  }
+  const onClick = useCallback(() => {
+    if (darkMode === theme.SYSTEM) {
+      // previous theme is system theme and toggle to respective theme
+      setDarkMode(() => {
+        Icon.current = prefersDarkMode ? ImSun : BsMoonFill
+        return prefersDarkMode ? theme.LIGHT : theme.DARK
+      })
+      return
+    }
+    // toggle to theme
+    setDarkMode(() => {
+      Icon.current = darkMode === theme.DARK ? ImSun : BsMoonFill
+      return darkMode === theme.DARK ? theme.LIGHT : theme.DARK
+    })
+  }, [prefersDarkMode, darkMode, setDarkMode])
 
-  let Icon = MdDevices
-  if (darkMode === theme.LIGHT) Icon = ImSun
-  if (darkMode === theme.DARK) Icon = BsMoonFill
+  useEffect(() => {
+    // on first render, set the icon to the stored theme
+    if (darkMode === theme.LIGHT) Icon.current = ImSun
+    if (darkMode === theme.DARK) Icon.current = BsMoonFill
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
   return (
     <Button type="button" className={styles.themeToggler} onClick={onClick}>
-      <Icon className={styles.icon} />
+      <Icon.current className={styles.icon} />
     </Button>
   )
 }
 
-export default ThemeToggle
+export default memo(ThemeToggle)
